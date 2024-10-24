758ea26d3e27d2d594964fb9f6ff1f2ff2b02905
Danielle Sim <dsi61@uclive.ac.nz>
2024-10-24 17:31:25 +1300

    initial commit

---
+++ Assets/Launcher.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public Rigidbody _prefabWithRigidbody;
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+#else
+        if (Input.touchCount > 0)
+#endif
+        {
+            Debug.Log("clicked or touched");
+
+            // spawn in front of at the camera
+            var pos = Camera.main.transform.position;
+            var forw = Camera.main.transform.forward;
+            var thing = Instantiate(_prefabWithRigidbody, pos + (forw * 0.4f), Quaternion.identity);
+
+            thing.AddForce(forw * 200.0f);  // shoot ball forward wuth a certain amount of velocity
+        }
+    }
+}
